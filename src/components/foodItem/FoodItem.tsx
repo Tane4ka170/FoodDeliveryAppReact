9f568f323e8e0b27a0867cdd76698d010ad4ba0d
Tane4ka170 <tane4ka170@gmail.com>
2024-12-06 16:48:32 +0200

    Added availability status and price to FoodItem

--- src/components/foodItem/FoodItem.tsx
+++ src/components/foodItem/FoodItem.tsx
@@ -9,9 +9,17 @@ interface Dish {
 }
 
 const FoodItem: React.FC<{ item: Dish }> = (props) => {
+  const isAvailable = props.item.status === "in-stock";
+
   return (
     <React.Fragment>
-      <div className={s.itemContainer}>{props.item.name}</div>
+      <div className={s.itemContainer}>
+        <div className={s.itemTitle}>{props.item.name}</div>
+        <div className={s.itemPrice}>{props.item.price}</div>
+        <div className={isAvailable ? s.itemAvailable : s.itemSold}>
+          {isAvailable ? "Available" : "Stock out"}
+        </div>
+      </div>
     </React.Fragment>
   );
 };
