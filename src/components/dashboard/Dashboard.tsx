d2e333b85f738ea764ea9d4c1d1089e55cac0fb1
Tane4ka170 <tane4ka170@gmail.com>
2024-12-06 15:38:13 +0200

    Created Dashboard component displaying food items

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import FoodItem from "../foodItem/FoodItem";
+
+interface Dish {
+  id: number;
+  name: string;
+  price: number;
+  status: string;
+}
+
+const foodItems: Dish[] = [
+  {
+    id: 1,
+    name: "Pizza Margherita",
+    price: 13,
+    status: "in-stock",
+  },
+  {
+    id: 2,
+    name: "Cheeseburger",
+    price: 9,
+    status: "in-stock",
+  },
+  {
+    id: 3,
+    name: "Caesar Salad",
+    price: 10,
+    status: "out-of-stock",
+  },
+  {
+    id: 4,
+    name: "Spaghetti Carbonara",
+    price: 15,
+    status: "in-stock",
+  },
+  {
+    id: 5,
+    name: "Sushi Platter",
+    price: 20,
+    status: "in-stock",
+  },
+  {
+    id: 6,
+    name: "Tiramisu",
+    price: 7,
+    status: "out-of-stock",
+  },
+];
+
+const Dashboard: React.FC<any> = (props) => {
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        {foodItems.map((item: Dish) => {
+          return <FoodItem item={item} />;
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Dashboard;
