13b8a3e6a4d75b8321213a88be759f9b13553034
Tane4ka170 <tane4ka170@gmail.com>
2024-12-07 14:42:32 +0200

    Added admin route & isLoggedIn state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Authenticate from "./components/authentication/Authenticate";
 import Dashboard from "./components/dashboard/Dashboard";
+import Admin from "./components/admin/Admin";
 
 const App: FC<any> = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   return (
     <Fragment>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Authenticate />} />
           <Route path="/" element={<Dashboard />} />
+          <Route path="/admin" element={<Admin isLoggedIn={isLoggedIn} />} />
         </Routes>
       </BrowserRouter>
     </Fragment>
