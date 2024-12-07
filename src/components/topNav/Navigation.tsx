d45f2d4eb8ee46fb9618643a08edb07b870657fa
Tane4ka170 <tane4ka170@gmail.com>
2024-12-07 16:55:02 +0200

    Added admin navigation & user type

--- src/components/topNav/Navigation.tsx
+++ src/components/topNav/Navigation.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import s from "./Navigation.module.css";
 
-const Navigation: React.FC<{ isLoggedIn: boolean }> = (props) => {
+type USER = "Customer" | "Admin";
+
+const Navigation: React.FC<{ isLoggedIn: boolean; user: USER }> = (props) => {
   return (
     <React.Fragment>
-      {props.isLoggedIn && props.user === "Custom" ? (
+      {props.isLoggedIn && props.user === "Customer" ? (
         <div className={s.navBarBg}>
           <ul className={s.navigationBar}>
             <li>Food Delivery</li>
@@ -17,6 +19,23 @@ const Navigation: React.FC<{ isLoggedIn: boolean }> = (props) => {
           <ul className={s.navigationBar}>
             <li>Food Delivery</li>
             <li>Login</li>
+          </ul>
+        </div>
+      )}
+      {props.isLoggedIn && props.user === "Admin" ? (
+        <div className={s.navBarBg}>
+          <ul className={s.navigationBar}>
+            <li>Food Delivery</li>
+            <li>Manage dishes</li>
+            <li>Manage customers</li>
+            <li>Logout</li>
+          </ul>
+        </div>
+      ) : (
+        <div className={s.navBarBg}>
+          <ul className={s.navigationBar}>
+            <li>Food Delivery</li>
+            <li>Admin panel</li>
           </ul>
         </div>
       )}
