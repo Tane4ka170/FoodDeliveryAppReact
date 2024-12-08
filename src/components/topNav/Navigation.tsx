36f4fd47017c8ac52f45105e71a1a5e0486d043b
Tane4ka170 <tane4ka170@gmail.com>
2024-12-08 16:14:15 +0200

    Refactored navigation logic into a separate function

--- src/components/topNav/Navigation.tsx
+++ src/components/topNav/Navigation.tsx
@@ -4,43 +4,44 @@ import s from "./Navigation.module.css";
 type USER = "Customer" | "Admin";
 
 const Navigation: React.FC<{ isLoggedIn: boolean; user: USER }> = (props) => {
-  return (
-    <React.Fragment>
-      {props.isLoggedIn && props.user === "Customer" ? (
+  function displayNavBar(user: USER) {
+    if (user === "Customer") {
+      return (
         <div className={s.navBarBg}>
           <ul className={s.navigationBar}>
             <li>Food Delivery</li>
-            <li>Manage orders</li>
-            <li>Logout</li>
+            {props.isLoggedIn ? (
+              <>
+                <li>Manage orders</li>
+                <li>Logout</li>
+              </>
+            ) : (
+              <li>Login</li>
+            )}
           </ul>
         </div>
-      ) : (
+      );
+    } else if (user === "Admin") {
+      return (
         <div className={s.navBarBg}>
           <ul className={s.navigationBar}>
             <li>Food Delivery</li>
-            <li>Login</li>
+            {props.isLoggedIn ? (
+              <>
+                <li>Manage dishes</li>
+                <li>Manage customers</li>
+                <li>Logout</li>
+              </>
+            ) : (
+              <li>Admin panel</li>
+            )}
           </ul>
         </div>
-      )}
-      {props.isLoggedIn && props.user === "Admin" ? (
-        <div className={s.navBarBg}>
-          <ul className={s.navigationBar}>
-            <li>Food Delivery</li>
-            <li>Manage dishes</li>
-            <li>Manage customers</li>
-            <li>Logout</li>
-          </ul>
-        </div>
-      ) : (
-        <div className={s.navBarBg}>
-          <ul className={s.navigationBar}>
-            <li>Food Delivery</li>
-            <li>Admin panel</li>
-          </ul>
-        </div>
-      )}
-    </React.Fragment>
-  );
+      );
+    }
+  }
+
+  return <React.Fragment>{displayNavBar(props.user)}</React.Fragment>;
 };
 
 export default Navigation;
