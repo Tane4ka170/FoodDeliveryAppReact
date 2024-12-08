0af32bf18bcb4a2b039872807abcd20155e0ed9b
Tane4ka170 <tane4ka170@gmail.com>
2024-12-08 16:01:30 +0200

    Refactored state, passed props to Dashboard/Admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { FC, Fragment, useState } from "react";
+import { FC, Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Authenticate from "./components/authentication/Authenticate";
 import Dashboard from "./components/dashboard/Dashboard";
 import Admin from "./components/admin/Admin";
 
 const App: FC<any> = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const isLoggedIn = false;
   return (
     <Fragment>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Authenticate />} />
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard isLoggedIn={isLoggedIn} />} />
           <Route path="/admin" element={<Admin isLoggedIn={isLoggedIn} />} />
         </Routes>
       </BrowserRouter>
