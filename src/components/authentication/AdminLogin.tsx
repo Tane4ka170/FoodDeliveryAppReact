422faed2605af55756b0798476b04d1f5984efe6
Tane4ka170 <tane4ka170@gmail.com>
2024-12-08 19:12:39 +0200

    Updated AdminLogin UI with new layout and added button

--- src/components/authentication/AdminLogin.tsx
+++ src/components/authentication/AdminLogin.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import s from "./Authentication.module.css";
+import Button from "../UI/button";
 
 const AdminLogin: React.FC<any> = (props) => {
   return (
     <React.Fragment>
-      <div className={s.adminLoginCart}>
-        <div className={s.input}>
-          <input type="text" />
-        </div>
-        <div className={s.input}>
-          <input type="password" />
+      <div className={s.centeredContainer}>
+        <div className={s.adminLoginCart}>
+          <h3 className={s.adminLoginTitle}>Admin Sign In</h3>
+          <input type="text" className={s.input} placeholder="Enter username" />
+
+          <input
+            type="password"
+            className={s.input}
+            placeholder="Enter password"
+          />
+          <Button color="white" bgColor="transparent" />
         </div>
       </div>
     </React.Fragment>
