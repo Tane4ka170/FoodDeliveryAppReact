da06c3e5de6c5e60be7c96d0cf5aed4a3fc0b973
Tane4ka170 <tane4ka170@gmail.com>
2024-12-09 19:40:10 +0200

    Display food items from API data

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -10,99 +10,6 @@ interface Dish {
   status: string;
 }
 
-const foodItems: Dish[] = [
-  {
-    id: 1,
-    name: "Pizza Margherita",
-    price: 13,
-    status: "in-stock",
-  },
-  {
-    id: 2,
-    name: "Cheeseburger",
-    price: 9,
-    status: "in-stock",
-  },
-  {
-    id: 3,
-    name: "Caesar Salad",
-    price: 10,
-    status: "out-of-stock",
-  },
-  {
-    id: 4,
-    name: "Spaghetti Carbonara",
-    price: 15,
-    status: "in-stock",
-  },
-  {
-    id: 5,
-    name: "Sushi Platter",
-    price: 20,
-    status: "in-stock",
-  },
-  {
-    id: 6,
-    name: "Tiramisu",
-    price: 7,
-    status: "out-of-stock",
-  },
-  {
-    id: 7,
-    name: "Grilled Chicken",
-    price: 12,
-    status: "in-stock",
-  },
-  {
-    id: 8,
-    name: "French Fries",
-    price: 5,
-    status: "in-stock",
-  },
-  {
-    id: 9,
-    name: "Vegetable Soup",
-    price: 8,
-    status: "out-of-stock",
-  },
-  {
-    id: 10,
-    name: "Lobster Bisque",
-    price: 25,
-    status: "in-stock",
-  },
-  {
-    id: 11,
-    name: "Steak",
-    price: 30,
-    status: "in-stock",
-  },
-  {
-    id: 12,
-    name: "Panna Cotta",
-    price: 8,
-    status: "in-stock",
-  },
-  {
-    id: 13,
-    name: "Apple Pie",
-    price: 6,
-    status: "in-stock",
-  },
-  {
-    id: 14,
-    name: "Fish Tacos",
-    price: 11,
-    status: "in-stock",
-  },
-  {
-    id: 15,
-    name: "Chocolate Lava Cake",
-    price: 9,
-    status: "out-of-stock",
-  },
-];
-
 const Dashboard: React.FC<any> = (props) => {
   return (
     <React.Fragment>
@@ -110,7 +17,7 @@ const Dashboard: React.FC<any> = (props) => {
       <div className={styles.dashboardContainer}>
         <h1 className={styles.menuTitle}>Menu Items</h1>
         <div className={styles.foodItemsGrid}>
-          {foodItems.map((item: Dish) => {
+          {props?.foodItems?.map((item: Dish) => {
             return <FoodItem item={item} />;
           })}
         </div>
