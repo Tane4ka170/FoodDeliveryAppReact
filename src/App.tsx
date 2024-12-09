6f4cbcc254900d1fdcc9220eabc23b65ea4c06e3
Tane4ka170 <tane4ka170@gmail.com>
2024-12-09 19:36:01 +0200

    Added food items state & API fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Authenticate from "./components/authentication/Authenticate";
 import Dashboard from "./components/dashboard/Dashboard";
@@ -6,12 +6,24 @@ import Admin from "./components/admin/Admin";
 
 const App: FC<any> = () => {
   const isLoggedIn = false;
+  const [foodItems, setFoodItems] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8259/food-items").then((items: any) => {
+      setFoodItems(items);
+    });
+  }, [foodItems, setFoodItems]);
   return (
     <Fragment>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Authenticate />} />
-          <Route path="/" element={<Dashboard isLoggedIn={isLoggedIn} />} />
+          <Route
+            path="/"
+            element={
+              <Dashboard isLoggedIn={isLoggedIn} foodItems={foodItems} />
+            }
+          />
           <Route path="/admin" element={<Admin isLoggedIn={isLoggedIn} />} />
         </Routes>
       </BrowserRouter>
