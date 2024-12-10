f6d48e56768133671b58e1b92f29ada19f1d6599
Tane4ka170 <tane4ka170@gmail.com>
2024-12-10 16:40:34 +0200

    Refactored App: added async fetch for foodItems

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,18 @@ import Admin from "./components/admin/Admin";
 
 const App: FC<any> = () => {
   const isLoggedIn = false;
-  const [foodItems, setFoodItems] = useState({});
+  const [foodItems, setFoodItems] = useState<
+    Array<{ id: number; name: string; price: number; status: string }>
+  >([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8259/food-items").then((items: any) => {
-      setFoodItems(items);
-    });
-  }, [foodItems, setFoodItems]);
+    const fetchData = async () => {
+      const response = await fetch("http://127.0.0.1:8259/food-items");
+      const data = response.json();
+      return data;
+    };
+    fetchData();
+  }, []);
   return (
     <Fragment>
       <BrowserRouter>
@@ -21,7 +26,14 @@ const App: FC<any> = () => {
           <Route
             path="/"
             element={
-              <Dashboard isLoggedIn={isLoggedIn} foodItems={foodItems} />
+              <Dashboard
+                isLoggedIn={isLoggedIn}
+                foodItems={
+                  foodItems.length > 0
+                    ? foodItems
+                    : [{ id: 0, name: "N/A", price: 0, status: "out-of-stock" }]
+                }
+              />
             }
           />
           <Route path="/admin" element={<Admin isLoggedIn={isLoggedIn} />} />
