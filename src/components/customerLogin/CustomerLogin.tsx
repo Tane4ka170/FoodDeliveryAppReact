6ae95490b33b096770857aff9f1281fcec8d868e
Tane4ka170 <tane4ka170@gmail.com>
2024-12-10 19:08:41 +0200

    ImplementedCustomerLoginComponent

--- src/components/customerLogin/CustomerLogin.tsx
+++ src/components/customerLogin/CustomerLogin.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import s from "./CustomerLogin.module.css";
 
 const CustomerLogin: React.FC<any> = (props) => {
-  return <React.Fragment>CustomerLofin</React.Fragment>;
+  return (
+    <React.Fragment>
+      <div className={s.centeredContainer}>
+        <div className={s.customerLoginCart}>
+          <h3 className={s.customerLoginTitle}>Customer Log In</h3>
+          <input
+            type="text"
+            placeholder="Enter your username here"
+            className={s.input}
+          />
+          <input
+            type="password"
+            placeholder="Enter your password here"
+            className={s.input}
+          />
+          <button className={s.loginButton}>Log in</button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default CustomerLogin;
