a04ae1f3ed044f6e3066b1b56a0e71868b7bf3b4
Tane4ka170 <tane4ka170@gmail.com>
2024-12-10 19:02:19 +0200

    Replaced Authenticate with CustomerLogin in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { FC, Fragment, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Authenticate from "./components/authentication/Authenticate";
 import Dashboard from "./components/dashboard/Dashboard";
 import Admin from "./components/admin/Admin";
 import CustomerLogin from "./components/customerLogin/CustomerLogin";
@@ -23,7 +22,7 @@ const App: FC<any> = () => {
     <Fragment>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Authenticate />} />
+          <Route path="/login" element={<CustomerLogin />} />
           <Route
             path="/"
             element={
@@ -38,7 +37,6 @@ const App: FC<any> = () => {
             }
           />
           <Route path="/admin" element={<Admin isLoggedIn={isLoggedIn} />} />
-          <Route path="/login" element={<CustomerLogin />} />
         </Routes>
       </BrowserRouter>
     </Fragment>
